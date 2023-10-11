07b123f4fe88b395ffea44a858fca923478c9c0f
Venaliss <venaliss2023@yandex.ru>
2023-10-11 23:49:04 +0300

    Создано тело метода

--- DZ_Tumakov/Program.cs
+++ DZ_Tumakov/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace DZ_Tumakov
 {
@@ -6,6 +7,32 @@ namespace DZ_Tumakov
     {
         static void Main(string[] args)
         {
+            
+            string str = File.ReadAllText("C:\\Users\\fifif\\source\\repos\\Home_Work_14_10_2023\\Tumakov.txt");
+            char[] ArrayChars = new char[str.Length];
+
+            for (int j = 0; j < str.Length - 1; j++)
+            {
+                ArrayChars[j] = str[j];
+            }
+
+            int Glas = 0;
+            int Sogl = 0;
+            string StrSogl = "QqWwRrTtPpSsDdFfGgHhJjKkLlZzXxCcVvBbNnMmЙйЦцКкНнГгШшЩщЗзХхФфВвПпРрЛлДдЧчСсМмТтБб";
+            string StrGlas = "EeYyUuIiOoAaУуЕеЫыАаОоЭэЯяИиЮю";
+            foreach (char arg in ArrayChars)
+            {
+                if (StrSogl.Contains(Convert.ToString(arg))) 
+                {
+                    Sogl++;
+                }
+                else if (StrGlas.Contains(Convert.ToString(arg))) 
+                {
+                    Glas++;
+                }
+            }
+            Console.WriteLine($"Количество согласных букв в файле равно - {Sogl} \nКоличество гласных букв в файле равно - {Glas}");
+            Console.ReadKey();
         }
     }
 }
